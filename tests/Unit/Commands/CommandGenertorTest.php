23e00205bbb20156894cb4bc6106a4d05e7740f4
Evaldo Klock <neto.nemesis@gmail.com>
2022-08-05 16:59:29 -0400

    fix tests

--- tests/Unit/Commands/CommandGenertorTest.php
+++ tests/Unit/Commands/CommandGenertorTest.php
@@ -8,25 +8,32 @@ use KlockTecnologia\KlockHelpers\Tests\TestCase;
 class CommandGenertorTest extends TestCase
 {
 
-    const DUMMY_CLASS_MODEL_PATH = __DIR__ . '/DummyClasses/Dummy.php';
-    const DUMMY_CLASS_CONTROLLER_PATH = __DIR__ . '/DummyClasses/DummyController.php';
-    const DUMMY_CLASS_SERVICE_PATH = __DIR__ . '/DummyClasses/DummyService.php';
+    const DUMMY_CLASS_MODEL_PATH = 'DummyClasses/Dummy.php';
+    const DUMMY_CLASS_CONTROLLER_PATH = 'DummyClasses/DummyController.php';
+    const DUMMY_CLASS_SERVICE_PATH = 'DummyClasses/DummyService.php';
 
     const GENERATED_DOMAIN_MODEL_PATH = 'Domains/Dummy/Models/Dummy.php';
     const GENERATED_DOMAIN_CONTROLLER_PATH = 'Domains/Dummy/Controllers/DummyController.php';
     const GENERATED_DOMAIN_SERVICE_PATH = 'Domains/Dummy/Services/DummyService.php';
 
+
+    protected static function getTestLocalPathFile($path)
+    {
+
+        return __DIR__ . "/" . $path;
+    }
+
     public function testDryModelGeneration($runningFromAnotherTest = false)
     {
 
-        $fooClass = app_path(self::DUMMY_CLASS_MODEL_PATH);
-
+        $fooClass = app_path(self::GENERATED_DOMAIN_MODEL_PATH);
 
         if (File::exists($fooClass) && !$runningFromAnotherTest) {
             unlink($fooClass);
+            $this->assertFalse(File::exists($fooClass));
+        } else {
+            $this->assertTrue(File::exists($fooClass));
         }
-
-        $this->assertFalse(File::exists($fooClass));
 
         if (!$runningFromAnotherTest) {
             $this->artisan('domain:model', ['name' => 'dummy']);
@@ -34,22 +41,22 @@ class CommandGenertorTest extends TestCase
 
         $this->assertTrue(File::exists($fooClass));
 
-        $expectedContents = file_get_contents(self::GENERATED_DOMAIN_MODEL_PATH);
+        $expectedContents = file_get_contents(self::getTestLocalPathFile(self::DUMMY_CLASS_MODEL_PATH));
 
         $this->assertEquals($expectedContents, file_get_contents($fooClass));
     }
 
-    @skip
+
     public function testServiceGeneration($runningFromAnotherTest = false)
     {
+        $fooClass = app_path(self::GENERATED_DOMAIN_SERVICE_PATH);
 
-        $fooClass = app_path(self::DUMMY_CLASS_SERVICE_PATH);
-
-        if (File::exists($fooClass) && $runningFromAnotherTest) {
+        if (File::exists($fooClass) && !$runningFromAnotherTest) {
             unlink($fooClass);
+            $this->assertFalse(File::exists($fooClass));
+        } else {
+            $this->assertTrue(File::exists($fooClass));
         }
-
-        $this->assertFalse(File::exists($fooClass));
 
         if (!$runningFromAnotherTest) {
 
@@ -58,7 +65,7 @@ class CommandGenertorTest extends TestCase
 
         $this->assertTrue(File::exists($fooClass));
 
-        $expectedContents = file_get_contents(self::GENERATED_DOMAIN_SERVICE_PATH);
+        $expectedContents = file_get_contents(self::getTestLocalPathFile(self::DUMMY_CLASS_SERVICE_PATH));
 
         $this->assertEquals($expectedContents, file_get_contents($fooClass));
     }
@@ -67,24 +74,21 @@ class CommandGenertorTest extends TestCase
     public function testControllerGeneration($runningFromAnotherTest = false)
     {
 
-        $fooClass = app_path(self::DUMMY_CLASS_CONTROLLER_PATH);
+        $fooClass = app_path(self::GENERATED_DOMAIN_CONTROLLER_PATH);
 
-        if (File::exists($fooClass) && $runningFromAnotherTest) {
+        if (File::exists($fooClass) && !$runningFromAnotherTest) {
             unlink($fooClass);
+            $this->assertFalse(File::exists($fooClass));
+        } else {
+            $this->assertTrue(File::exists($fooClass));
         }
-
-        $this->assertFalse(File::exists($fooClass));
-
         if (!$runningFromAnotherTest) {
             $this->artisan('domain:controller', ['name' => 'dummy']);
         }
 
         $this->assertTrue(File::exists($fooClass));
 
-        $expectedContents = file_get_contents(self::GENERATED_DOMAIN_CONTROLLER_PATH);
-
-        dd($expectedContents, file_get_contents($fooClass));
-
+        $expectedContents = file_get_contents(self::getTestLocalPathFile(self::DUMMY_CLASS_CONTROLLER_PATH));
 
         $this->assertEquals($expectedContents, file_get_contents($fooClass));
     }
@@ -92,9 +96,9 @@ class CommandGenertorTest extends TestCase
     public function testGenerateCompleteDomain()
     {
 
-        // $this->artisan('domain:make', ['name' => 'dummy']);
-        // $this->testDryModelGeneration(true);
-        // $this->testControllerGeneration(true);
-        // $this->testServiceGeneration(true);
+        $this->artisan('domain:make', ['name' => 'dummy']);
+        $this->testDryModelGeneration(true);
+        $this->testControllerGeneration(true);
+        $this->testServiceGeneration(true);
     }
 }
